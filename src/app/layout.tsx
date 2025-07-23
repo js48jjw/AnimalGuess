11fc8b0fafa6e57630593ae6e9b65adbde8e671a
JJW <stickbox@naver.com>
2025-07-23 13:38:46 +0900

    no ads

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,7 +11,9 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         <title>동물을 맞춰라!</title>
       </head>
       <body>
+        {/*
         {/* 좌측 고정 광고 */}
+        {/*
         <div style={{
           position: 'fixed',
           left: 5,
@@ -31,7 +33,9 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
           ></ins>
           <Script src="//t1.daumcdn.net/kas/static/ba.min.js" strategy="afterInteractive" />
         </div>
+        */}
         {/* 우측 고정 광고 */}
+        {/*
         <div style={{
           position: 'fixed',
           right: 16,
@@ -51,6 +55,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
           ></ins>
           <Script src="//t1.daumcdn.net/kas/static/ba.min.js" strategy="afterInteractive" />
         </div>
+        */}
         {/* 메인 컨텐츠 */}
         {children}
       </body>
